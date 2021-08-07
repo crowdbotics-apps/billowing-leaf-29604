d15cd2ac47a571ae266380f3f26aa83adccb5cbd
Crowdbotics <team@crowdbotics.com>
2021-08-07 12:26:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_leaf_29604Identifier",
+  url: "https://billowing_leaf_29604Identifier.botics.co",
+  api: "https://billowing_leaf_29604Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
